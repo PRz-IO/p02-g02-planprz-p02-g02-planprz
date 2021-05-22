629acb796ed5b90051734defb06fcf9b7971bbed
Thebroscable <57013863+Thebroscable@users.noreply.github.com>
2021-05-22 15:18:25 +0200

    strona home

--- PRzMapa/PRzMapa/settings.py
+++ PRzMapa/PRzMapa/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'PRzMapa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["PRzMapa/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,10 +73,23 @@ WSGI_APPLICATION = 'PRzMapa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'plan_prz',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '',
     }
 }
 
--- PRzMapa/myapp/admin.py
+++ PRzMapa/myapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Administratorzy)
+admin.site.register(Punkt)
+admin.site.register(Pracownicy)
+admin.site.register(DniTygodnia)
+admin.site.register(GodzinyOtwarcia)
+admin.site.register(Kategoria)
+admin.site.register(PunktPracownicy)
+admin.site.register(Obiekt)
