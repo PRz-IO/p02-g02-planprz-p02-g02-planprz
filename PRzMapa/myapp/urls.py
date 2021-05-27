79a8014c2c052c0868fed39db4cc5ce198da3010
JohnWolf12 <janek_wilk@onet.pl>
2021-05-27 22:06:21 +0200

    aktualizacja

--- PRzMapa/myapp/urls.py
+++ PRzMapa/myapp/urls.py
@@ -3,8 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
-    path('budynki/', budynki, name='budynki'),
+    path('budynki/', obiekty, name='budynki'),
     path('punkty/', punkty, name='punkty'),
+    path('budynki/<id>/', punkty_b, name='punkty_b'),
 
 ]
 
--- PRzMapa/myapp/views.py
+++ PRzMapa/myapp/views.py
@@ -9,8 +9,16 @@ def index(request):
     return render(request, "home.html", context)
 
 
-def budynki(request):
-    return render(request, "budynki.html")
-
 def punkty(request):
     return render(request, "punkty.html")
+
+def obiekty(request):
+    obiekty_user = Obiekt.objects.all()
+    dane= {'obiekty_user': obiekty_user}
+    return render(request,'budynki.html',dane)
+
+def punkty_b(request, id):
+    obiekt_user = Obiekt.objects.get(pk=id)
+    punkty_budynek = Punkt.objects.filter(obiekt_id_obiektu=id)
+    dane= {'punkty_budynek': punkty_budynek, 'obiekt_user': obiekt_user}
+    return render(request,'budynek.html',dane)
