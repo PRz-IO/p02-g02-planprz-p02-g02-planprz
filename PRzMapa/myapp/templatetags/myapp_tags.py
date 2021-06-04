b4b728a1aebae47dca4542c4d41d00d4248ffc37
Thebroscable <57013863+Thebroscable@users.noreply.github.com>
2021-06-04 18:56:08 +0200

    punkty poprawione + mapa 1/3

--- PRzMapa/myapp/templatetags/myapp_tags.py
+++ PRzMapa/myapp/templatetags/myapp_tags.py
@@ -3,9 +3,5 @@ from django import template
 register = template.Library()
 
 @register.filter
-def in_id(things, id):
-    return things.filter(dni_tygodnia_id_dnia_tygodnia=id)
-
-@register.filter
-def get_att(things):
-    return getattr(things, 'godz_otw')
+def link(obraz):
+    return obraz.decode("utf-8")
