ab5ba1cb9dd0e9c19c2d985cacbe8cc68dc44d53
lucusiaaa <lucjapalkus@gmail.com>
2021-06-20 22:33:39 +0200

    tabele w adminie

--- PRzMapa/myapp/admin.py
+++ PRzMapa/myapp/admin.py
@@ -15,25 +15,25 @@ from django.contrib import admin
 #         return "{} {}".format(obj.nazwisko, obj.imię)
 #
 #
-# class DniTygodniaAdmin(admin.ModelAdmin):
-#     list_display = ("id_dnia_tygodnia","dzień")
-#
-#     def has_add_permission(self, request, obj=None):
-#         return False
-#
-#
-# class InLineGodzinyOtwarcia(admin.StackedInline):
-#     model = GodzinyOtwarcia
-#     max_num = 7
-#
-#
-# class PunktAdmin(admin.ModelAdmin):
-#     inlines = [InLineGodzinyOtwarcia]
-#     list_display = ("id_punktu", "nazwa")
-#     list_filter = ('kategoria_id_kategorii',)
-#     search_fields = ('nazwa', 'informacje')
-#
-#
+class DniTygodniaAdmin(admin.ModelAdmin):
+    list_display = ("id","dzień")
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+
+class InLineGodzinyOtwarcia(admin.StackedInline):
+    model = GodzinyOtwarcia
+    max_num = 7
+
+
+class PunktAdmin(admin.ModelAdmin):
+    inlines = [InLineGodzinyOtwarcia]
+    list_display = ("id", "nazwa")
+    list_filter = ('kategoria_id_kategorii',)
+    search_fields = ('nazwa', 'informacje')
+
+
 # class PracownicyAdmin(admin.ModelAdmin):
 #     list_display = ("id_pracownika", 'Nazwisko_i_imię', 'adres_mailowy', 'kontakt', 'data_założenia_konta', 'czy_aktywowany')
 #     search_fields = ('imię', 'nazwisko', 'adres_mailowy', 'kontakt')
@@ -41,34 +41,35 @@ from django.contrib import admin
 #
 #     def Nazwisko_i_imię(self, obj):
 #         return "{} {}".format(obj.nazwisko, obj.imię)
-#
-#
-# class GodzinyOtwarciaAdmin(admin.ModelAdmin):
-#     list_display = ("dni_tygodnia_id_dnia_tygodnia", "punkt_id_punktu")
-#     list_filter = ('dni_tygodnia_id_dnia_tygodnia',)
-#     fields = ('dni_tygodnia_id_dnia_tygodnia', 'punkt_id_punktu', ('godz_otw', 'godz_zamkn'))
-#
-# class KategoriaAdmin(admin.ModelAdmin):
-#     list_display = ("id_kategorii", "nazwa_kategorii")
-#     list_filter = ('nazwa_kategorii',)
-#
-#
-# class PunktPracownicyAdmin(admin.ModelAdmin):
-#     list_display = ("punkt_id_punktu", "pracownicy_id_pracownika")
-#     list_filter = ('punkt_id_punktu', 'pracownicy_id_pracownika')
-#
-#
-# class ObiektAdmin(admin.ModelAdmin):
-#     list_display = ("id_obiektu", "nazwa",'adres','ułatwienia_dla_niepełnosprawnych')
-#     list_filter = ('ułatwienia_dla_niepełnosprawnych',)
-#     search_fields = ('nazwa', 'adres')
-#
-#
+
+
+class GodzinyOtwarciaAdmin(admin.ModelAdmin):
+    list_display = ("dni_tygodnia_id_dnia_tygodnia", "punkt_id_punktu")
+    list_filter = ('dni_tygodnia_id_dnia_tygodnia',)
+    fields = ('dni_tygodnia_id_dnia_tygodnia', 'punkt_id_punktu', ('godz_otw', 'godz_zamkn'))
+
+class KategoriaAdmin(admin.ModelAdmin):
+    list_display = ("id", "nazwa_kategorii")
+    list_filter = ('nazwa_kategorii',)
+
+
+class PunktPracownicyAdmin(admin.ModelAdmin):
+    list_display = ("punkt_id_punktu", "pracownicy_id_pracownika")
+    list_filter = ('punkt_id_punktu', 'pracownicy_id_pracownika')
+
+
+class ObiektAdmin(admin.ModelAdmin):
+    list_display = ("id", "nazwa",'adres','ułatwienia_dla_niepełnosprawnych')
+    list_filter = ('ułatwienia_dla_niepełnosprawnych',)
+    search_fields = ('nazwa', 'adres')
+
+
 #admin.site.register(Administratorzy)
-admin.site.register(DniTygodnia)
-admin.site.register(Punkt)
+admin.site.register(DniTygodnia, DniTygodniaAdmin)
+admin.site.register(Punkt, PunktAdmin)
+# admin.site.register(Pracownicy, PracownicyAdmin)
 admin.site.register(Pracownicy)
-admin.site.register(GodzinyOtwarcia)
-admin.site.register(Kategoria)
-admin.site.register(PunktPracownicy)
-admin.site.register(Obiekt)
+admin.site.register(GodzinyOtwarcia, GodzinyOtwarciaAdmin)
+admin.site.register(Kategoria, KategoriaAdmin)
+admin.site.register(PunktPracownicy, PunktPracownicyAdmin)
+admin.site.register(Obiekt, ObiektAdmin)
